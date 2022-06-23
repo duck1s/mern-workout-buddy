63b2812df277062ec219bc37d4ce9bb0bcf5c1b2
Duck1s <joeri.breedveld123@gmail.com>
2022-06-23 16:24:22 +0200

    mongodb atlas and mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // Create express app
@@ -17,7 +18,15 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/workouts', workoutRoutes)
 
-// Listen for requests
-app.listen(process.env.PORT, () => {
-	console.log('listening on port', process.env.PORT)
-})
+// Coonnect to database
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		// Listen for requests
+		app.listen(process.env.PORT, () => {
+			console.log('connected to db and listening on port', process.env.PORT)
+		})
+	})
+	.catch((error) => {
+		console.log(error)
+	})
