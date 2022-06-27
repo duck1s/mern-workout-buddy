46e0228305ef667db0f779989baa98ce894d2075
Duck1s <joeri.breedveld123@gmail.com>
2022-06-27 19:47:40 +0200

    new workout form

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
 
 // Components
-import WorkoutDetails from '../components/workoutDetails'
+import WorkoutDetails from "../components/workoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
 	const [workouts, setWorkouts] = useState(null)
 
 	useEffect(() => {
 		const fetchWorkouts = async () => {
-			const response = await fetch('/api/workouts')
+			const response = await fetch("/api/workouts")
 			const json = await response.json()
 
 			if (response.ok) {
@@ -22,6 +23,7 @@ const Home = () => {
 	return (
 		<div className='home'>
 			<div className='workouts'>{workouts && workouts.map((workout) => <WorkoutDetails key={workout._id} workout={workout} />)}</div>
+			<WorkoutForm />
 		</div>
 	)
 }
